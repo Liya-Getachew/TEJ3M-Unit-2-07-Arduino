b4f9e1875bc28ae3c95f2d135be402af9376fd01
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2025-04-01 10:03:27 -0400

    code

---
+++ code.c
@@ -0,0 +1,48 @@
+/*
+ * Created by Liya G.
+ * Created on March 25, 2025
+ 
+ * Uses a sonar with a servo
+ */
+
+ # include <Servo.h>
+
+
+ const int TRIG_PIN = 3;  
+ const int ECHO_PIN = 2; 
+ const float SPEED_OF_LIGHT = 0.0343;
+ const int DISTANCE_TO_MOVE = 50;
+ 
+ Servo servoNumber1;
+   
+ float duration, distance;
+ 
+ void setup() {
+   pinMode(TRIG_PIN, OUTPUT);
+   pinMode(ECHO_PIN, INPUT);
+   Serial.begin(9600);
+   servoNumber1.attach(4);
+   servoNumber1.write(0);
+ }
+ 
+ void loop() {
+   servoNumber1.write(0);
+   digitalWrite(TRIG_PIN, LOW);
+   delayMicroseconds(2);
+   digitalWrite(TRIG_PIN, HIGH);
+   delayMicroseconds(10);
+   digitalWrite(TRIG_PIN, LOW);
+ 
+   duration = pulseIn(ECHO_PIN, HIGH);
+   distance = (duration*SPEED_OF_LIGHT)/2;
+   Serial.print("Distance: ");
+   Serial.println(distance);
+   delay(100);
+   
+   if (distance < DISTANCE_TO_MOVE) {
+     servoNumber1.write(0);
+     delay(1000);
+     servoNumber1.write(90);
+     delay(1000);
+   }
+ }
