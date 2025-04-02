fba9dc314e98d5576cd7aa20052b638cafe8666a
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2025-04-02 09:34:53 -0400

    comments

--- code.c
+++ code.c
@@ -27,19 +27,24 @@
  }
  
  void loop() {
+  // clears trig pin
    servoNumber1.write(0);
    digitalWrite(TRIG_PIN, LOW);
+
+  // set trig in on high fo 10 microseconds
    delayMicroseconds(2);
    digitalWrite(TRIG_PIN, HIGH);
    delayMicroseconds(10);
    digitalWrite(TRIG_PIN, LOW);
  
+   // detects distance
    duration = pulseIn(ECHO_PIN, HIGH);
    distance = (duration*SPEED_OF_LIGHT)/2;
    Serial.print("Distance: ");
    Serial.println(distance);
    delay(100);
    
+   // if distance is less than 50cm, move the servo 90 degrees
    if (distance < DISTANCE_TO_MOVE) {
      servoNumber1.write(0);
      delay(1000);
